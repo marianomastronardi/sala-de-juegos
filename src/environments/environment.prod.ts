f14bc6d195356f63a89107f27a443306858a53c5
Your Name <you@example.com>
2021-03-28 22:15:40 -0300

    sprint 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB18GjKPCxkSfg3X4EccyMalGV7lyC9ki8",
+    authDomain: "sala-de-juegos-108050.firebaseapp.com",
+    databaseURL: "https://sala-de-juegos-108050-default-rtdb.firebaseio.com",
+    projectId: "sala-de-juegos-108050",
+    storageBucket: "sala-de-juegos-108050.appspot.com",
+    messagingSenderId: "215134952047",
+    appId: "1:215134952047:web:8bb20caf869b48212acb9a",
+    measurementId: "G-TBB35LXPMP"
+  }
 };
